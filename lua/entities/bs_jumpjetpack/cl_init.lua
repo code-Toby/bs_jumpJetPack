bed3639e1758a14eb49a101041089d3e2723a543
CodeToby <wahfox8@gmail.com>
2026-01-25 15:43:20 -0500

    added the right way to check for valid particle systems 	modified:   lua/entities/bs_jumpjetpack/cl_init.lua

--- lua/entities/bs_jumpjetpack/cl_init.lua
+++ lua/entities/bs_jumpjetpack/cl_init.lua
@@ -19,21 +19,21 @@ function ENT:Think()
             self.thrust_eff_r = CreateParticleSystem( self, 'rockettrail_airstrike', PATTACH_CUSTOMORIGIN)
         end
 
-        if self.thrust_eff_l != nil then
+        if self.thrust_eff_l:IsValid()then
             self.thrust_eff_l:SetControlPoint(0, self:LocalToWorld(Vector(-1,4.5,-10)))
             self.thrust_eff_l:SetControlPointForwardVector( 0, -self:GetUp())
         end
 
-        if self.thrust_eff_r != nil then
+        if self.thrust_eff_r:IsValid() then
             self.thrust_eff_r:SetControlPoint(0, self:LocalToWorld(Vector(-1,-4.5,-10)))
             self.thrust_eff_r:SetControlPointForwardVector( 0, -self:GetUp())
         end
     else
-        if self.thrust_eff_l != nil then
+        if self.thrust_eff_l:IsValid() then
             self.thrust_eff_l:StopEmission(true, false, true)
             self.thrust_eff_l = nil
         end
-        if self.thrust_eff_r != nil then
+        if self.thrust_eff_r:IsValid() then
             self.thrust_eff_r:StopEmission(true, false, true)
             self.thrust_eff_r = nil
         end
