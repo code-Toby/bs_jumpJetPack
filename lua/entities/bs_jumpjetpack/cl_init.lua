6973ed0540cc27bb61efeed2b404c31b5f33044c
CodeToby <wahfox8@gmail.com>
2026-01-26 06:09:25 -0500

    quick fix 	modified:   lua/entities/bs_jumpjetpack/cl_init.lua

--- lua/entities/bs_jumpjetpack/cl_init.lua
+++ lua/entities/bs_jumpjetpack/cl_init.lua
@@ -19,23 +19,31 @@ function ENT:Think()
             self.thrust_eff_r = CreateParticleSystem( self, 'rockettrail_airstrike', PATTACH_CUSTOMORIGIN)
         end
 
-        if self.thrust_eff_l:IsValid()then
-            self.thrust_eff_l:SetControlPoint(0, self:LocalToWorld(Vector(-1,4.5,-10)))
-            self.thrust_eff_l:SetControlPointForwardVector( 0, -self:GetUp())
+        if self.thrust_eff_l:IsValid() then
+            if self.thrust_eff_l != nil then
+                self.thrust_eff_l:SetControlPoint(0, self:LocalToWorld(Vector(-1,4.5,-10)))
+                self.thrust_eff_l:SetControlPointForwardVector( 0, -self:GetUp())
+            end
         end
 
         if self.thrust_eff_r:IsValid() then
-            self.thrust_eff_r:SetControlPoint(0, self:LocalToWorld(Vector(-1,-4.5,-10)))
-            self.thrust_eff_r:SetControlPointForwardVector( 0, -self:GetUp())
+            if self.thrust_eff_r != nil then
+                self.thrust_eff_r:SetControlPoint(0, self:LocalToWorld(Vector(-1,-4.5,-10)))
+                self.thrust_eff_r:SetControlPointForwardVector( 0, -self:GetUp())
+            end
         end
     else
         if self.thrust_eff_l:IsValid() then
-            self.thrust_eff_l:StopEmission(true, false, true)
-            self.thrust_eff_l = nil
+            if self.thrust_eff_l != nil then
+                self.thrust_eff_l:StopEmission(true, false, true)
+                self.thrust_eff_l = nil
+            end
         end
         if self.thrust_eff_r:IsValid() then
-            self.thrust_eff_r:StopEmission(true, false, true)
-            self.thrust_eff_r = nil
+            if self.thrust_eff_r != nil then
+                self.thrust_eff_r:StopEmission(true, false, true)
+                self.thrust_eff_r = nil
+            end
         end
     end
 end
