6a0c32a9560d37f30058a1691c4ba9017cc6afec
CodeToby <wahfox8@gmail.com>
2026-01-26 18:53:48 -0500

    fixed my fix (i was sleepy q~q)
    	modified:   lua/entities/bs_jumpjetpack/cl_init.lua

--- lua/entities/bs_jumpjetpack/cl_init.lua
+++ lua/entities/bs_jumpjetpack/cl_init.lua
@@ -19,28 +19,28 @@ function ENT:Think()
             self.thrust_eff_r = CreateParticleSystem( self, 'rockettrail_airstrike', PATTACH_CUSTOMORIGIN)
         end
 
-        if self.thrust_eff_l:IsValid() then
-            if self.thrust_eff_l != nil then
+        if self.thrust_eff_l != nil then
+            if self.thrust_eff_l:IsValid() then
                 self.thrust_eff_l:SetControlPoint(0, self:LocalToWorld(Vector(-1,4.5,-10)))
                 self.thrust_eff_l:SetControlPointForwardVector( 0, -self:GetUp())
             end
         end
 
-        if self.thrust_eff_r:IsValid() then
-            if self.thrust_eff_r != nil then
+        if self.thrust_eff_r != nil then
+            if self.thrust_eff_r:IsValid() then
                 self.thrust_eff_r:SetControlPoint(0, self:LocalToWorld(Vector(-1,-4.5,-10)))
                 self.thrust_eff_r:SetControlPointForwardVector( 0, -self:GetUp())
             end
         end
     else
-        if self.thrust_eff_l:IsValid() then
-            if self.thrust_eff_l != nil then
+        if self.thrust_eff_l != nil then
+            if self.thrust_eff_l:IsValid() then
                 self.thrust_eff_l:StopEmission(true, false, true)
                 self.thrust_eff_l = nil
             end
         end
-        if self.thrust_eff_r:IsValid() then
-            if self.thrust_eff_r != nil then
+        if self.thrust_eff_r != nil then
+            if self.thrust_eff_r:IsValid() then
                 self.thrust_eff_r:StopEmission(true, false, true)
                 self.thrust_eff_r = nil
             end
